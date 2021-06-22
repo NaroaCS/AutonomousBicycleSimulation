e33199865e200acdd183119b9bb091e04549307b
imartinezl <inigomlap@gmail.com>
2021-06-22 09:49:34 +0200

    Analysis: station-based

--- analysis/stationbased_single.R
+++ analysis/stationbased_single.R
@@ -1,10 +1,12 @@
 library(magrittr)
+Sys.setlocale("LC_ALL", 'en_US.UTF-8')
+Sys.setenv(LANG = "en_US.UTF-8")
 
 # path_experiments <- "../results/station-based-23-03-2021/"
 # path_dir <- list.files(path_experiments, full.names = T)
 
 path <- "../results/2021-04-02_17-46-44/"
-path <- "../results/2021-04-06_16-53-31/"
+path <- "../results/station-based-03-04-2021/2021-04-02_18-20-43/"
 
 config <- jsonlite::fromJSON(file.path(path, "config.json"))
 user_trips <- data.table::fread(file.path(path, "user_trips.csv"), na.strings = "None")
@@ -27,9 +29,15 @@ user_trips$magic_dock %>% sum
 # DEMAND ------------------------------------------------------------------
 
 origin <- "2019-10-07"
-freq <- 15*60
+freq <- 30*60
 freq_hour <- 60*60
 freq_day <- 24*60*60
+date_breaks <- seq(lubridate::ymd_hm('2019-10-07 12:00'),lubridate::ymd_hm('2019-10-14 12:00'),by='day')
+date_minor_breaks <- seq(lubridate::ymd_hm('2019-10-07 00:00'),lubridate::ymd_hm('2019-10-15 00:00'),by='12 hours')
+
+
+width <- 6
+height <- 4
 
 # demand along the week
 user_trips %>% 
@@ -43,9 +51,24 @@ user_trips %>%
   ) %>% 
   ggplot2::ggplot()+
   ggplot2::geom_line(ggplot2::aes(x=date, y=count))+
-  ggplot2::scale_x_datetime(date_labels = "%d-%m-%Y", date_breaks = "1 day", date_minor_breaks = "1 hour")+
-  ggplot2::ggtitle(paste0("Demand: trip count every ", freq/60, " minutes"))+
-  ggplot2::theme_bw()
+  ggplot2::scale_x_datetime(date_labels = "%A", breaks = date_breaks, minor_breaks = date_minor_breaks)+
+  # ggplot2::ggtitle(paste0("Demand: trip count every ", freq/60, " minutes"))+
+  ggplot2::labs(x=NULL, y="Number of Trips", color="Weekday")+
+  # ggplot2::theme_bw()+
+  hrbrthemes::theme_ipsum(base_family = "Ubuntu", axis_title_just = "c")+
+  ggplot2::theme(
+    plot.margin = grid::unit(c(0, 0, 0, 0), "null"),
+    panel.spacing = grid::unit(c(0, 0, 0, 0), "null"),
+    legend.title = ggplot2::element_text(size=10),
+    legend.position = c(0.9,0.76),
+    legend.background = ggplot2::element_rect(fill = "white", colour = NA),
+    axis.title.x = ggplot2::element_text(size=10),
+    axis.text.x = ggplot2::element_text(size=8),
+    axis.title.y = ggplot2::element_text(size=10),
+    axis.text.y = ggplot2::element_text(size=8),
+    panel.grid.major.x = ggplot2::element_blank(),
+  )+
+  ggplot2::ggsave(filename = "demand_1.pdf", width=width, height=height)
 
 # demand per day
 user_trips %>% 
@@ -60,10 +83,24 @@ user_trips %>%
     date_day = hms::as_hms(date) %>% as.POSIXct(origin=origin),
   ) %>% 
   ggplot2::ggplot()+
-  ggplot2::geom_line(ggplot2::aes(x=date_day, y=count, color=day))+
+  ggplot2::geom_line(ggplot2::aes(x=date_day, y=count, color=day), size=0.5)+
   ggplot2::scale_x_datetime(date_labels = "%H:%M", date_breaks = "2 hours")+
-  ggplot2::ggtitle(paste0("Demand: trip count every ", freq/60, " minutes, per day"))+
-  ggplot2::theme_bw()
+  ggsci::scale_color_locuszoom()+
+  # ggplot2::ggtitle(paste0("Demand: trip count every ", freq/60, " minutes, per day"))+
+  ggplot2::labs(x="Time [HH:MM]", y="Number of Trips", color="Weekday")+
+  hrbrthemes::theme_ipsum(base_family = "Ubuntu", axis_title_just = "c")+
+  ggplot2::theme(
+    plot.margin = grid::unit(c(0, 0, 0, 0), "null"),
+    panel.spacing = grid::unit(c(0, 0, 0, 0), "null"),
+    legend.title = ggplot2::element_text(size=10),
+    legend.position = c(0.9,0.73),
+    legend.background = ggplot2::element_rect(fill = "white", colour = NA),
+    axis.title.x = ggplot2::element_text(size=10),
+    axis.text.x = ggplot2::element_text(size=8),
+    axis.title.y = ggplot2::element_text(size=10),
+    axis.text.y = ggplot2::element_text(size=8),
+    )+
+  ggplot2::ggsave(filename = "demand_2.pdf", width=width, height=height)
 
 
 # demand air distance
@@ -101,7 +138,7 @@ user_trips %>%
 
 # trip air distance: boxplot by distance
 breaks <- seq(0, 15000, by=1000)
-labels <- mapply(paste0, breaks, "-", breaks[-1], " km")[-1]
+labels <- mapply(paste0, breaks/1000, "-", breaks[-1]/1000, " km")[-1]
 user_trips %>% 
   dplyr::mutate(
     trip_air_distance = haversine(deg2rad(origin_lon), deg2rad(origin_lat), 
@@ -109,10 +146,40 @@ user_trips %>%
     trip_air_distance = cut(trip_air_distance, breaks, labels)
   ) %>% 
   dplyr::count(trip_air_distance) %>% 
+  na.omit() %>% 
+  dplyr::mutate(
+    n_cumsum = cumsum(n),
+    n_cumsum = n_cumsum / max(n_cumsum),
+    g = 1
+  ) %>% 
   ggplot2::ggplot()+
-  ggplot2::geom_col(ggplot2::aes(x=trip_air_distance, y=n), na.rm=T)+
-  ggplot2::ggtitle("Demand: trip air-distance, by km")+
-  ggplot2::theme_bw()
+  ggplot2::geom_col(ggplot2::aes(x=trip_air_distance, y=n, color = "Number_of_Trips"), width=0.7, fill="#343A40", na.rm=T)+
+  ggplot2::geom_line(ggplot2::aes(x=trip_air_distance, y=n_cumsum*25000, group=g, color="Cumulative_Distribution"),
+                     size=1, linetype="solid", na.rm=T)+
+  ggplot2::geom_point(ggplot2::aes(x=trip_air_distance, y=n_cumsum*25000, color="Cumulative_Distribution"),
+                     size=2, shape=15, na.rm=T)+
+  ggplot2::scale_y_continuous(
+    sec.axis = ggplot2::sec_axis(trans = ~ ./250, name = "Cumulative Distribution [%]", breaks = seq(0,100,by=10), )
+  )+
+  ggplot2::scale_color_manual(labels = c("Cumulative Distribution [%]","Number of Trips"), 
+                              values = c("#01937C", "#343A40"))+
+  ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(fill = "white"))) +
+  # ggplot2::ggtitle("Demand: trip air-distance, by km")+
+  ggplot2::theme_bw()+
+  ggplot2::labs(x="Trip Air Distance [km]", y="Number of Trips", color=NULL)+
+  hrbrthemes::theme_ipsum(base_family = "Ubuntu", axis_title_just = "c")+
+  ggplot2::theme(
+    plot.margin = grid::unit(c(0, 0, 0, 0), "null"),
+    panel.spacing = grid::unit(c(0, 0, 0, 0), "null"),
+    legend.title = ggplot2::element_text(size=10),
+    legend.position = c(0.78,0.76),
+    legend.background = ggplot2::element_rect(fill = "white", colour = NA),
+    axis.title.x = ggplot2::element_text(size=10),
+    axis.text.x = ggplot2::element_text(size=8),
+    axis.title.y = ggplot2::element_text(size=10),
+    axis.text.y = ggplot2::element_text(size=8),
+  )+
+  ggplot2::ggsave(filename = "demand_3.pdf", width=width, height=height)
 
 # trips status: boxplot by distance, colored by status
 user_trips %>% 
@@ -319,14 +386,6 @@ user_trips %>%
   ggplot2::scale_x_datetime(date_labels = "%d-%m-%Y", date_breaks = "1 day", date_minor_breaks = "1 hour")+
   ggplot2::ggtitle("Bikes activities: by discrete events")+
   ggplot2::theme_bw()
-
-
-
-# VEHICLES ----------------------------------------------------------------
-
-# vehicle kms travel
-riding_speed <- config$RIDING_SPEED
-
 
 
 
