78cc405930cd0732dec776936d450f60879cd268
NaroaCS <naroa.coretti@gmail.com>
2021-07-21 14:27:10 -0400

    first edits to doc

--- docs/docs/intro.md
+++ docs/docs/intro.md
@@ -2,34 +2,43 @@
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# Getting Started
 
-Let's discover **Docusaurus in less than 5 minutes**.
-
-## Getting Started
-
-Get started by **creating a new site**.
-
-Or **try Docusaurus immediately** with **[new.docusaurus.io](https://new.docusaurus.io)**.
-
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**:
+Before you start, you will need to download the code from our **[GitHub repo](https://guides.github.com/)**. If you are new to GitHub, **[here's](http://rogerdudler.github.io/git-guide/)** a simple guide to get you started. To get our code run:
 
 ```shell
-npx @docusaurus/init@latest init my-website classic
+git clone https://github.com/NaroaCS/AutonomousBicycleSimulation.git
 ```
 
-## Start your site
-
-Run the development server:
+Make sure you have Python 3 or higher. We're using Python 3.6. To check your python version:
 
 ```shell
-cd my-website
-
-npx docusaurus start
+python3 --version
 ```
 
-Your site starts at `http://localhost:3000`.
+If you do not have Python 3, you can donwload it from  **[Python.org](https://www.python.org/downloads/)**. 
 
-Open `docs/intro.md` and edit some lines: the site **reloads automatically** and display your changes.
+Finally, you will need some packages:
+
+* simpy
+* pandas
+* numpy
+* matplotlib
+* networkx
+* scipy==1.6.0
+* geopandas
+* scikit-learn
+* pyproj
+* git+git://github.com/imartinezl/pandana.git@master
+
+To install the packages you can use pip3, which comes already ingtegated with Python3. Eg:
+
+```shell
+pip3 install numpy
+```
+
+Alternatively, you can also use **[Anaconda](https://www.anaconda.com/)**, which can help you to handle packages and libraries. In that case, you would use conda to install the packages. Eg: 
+
+```shell
+conda install numpy
+```
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -3,18 +3,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Micro-mobility ABM',
+  tagline: 'Simulating future mobility modes',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'MIT Media Lab - City Science', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Micro-mobility ABM',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -28,7 +28,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/NaroaCS/AutonomousBicycleSimulation',
           label: 'GitHub',
           position: 'right',
         },
@@ -45,37 +45,7 @@ module.exports = {
               to: '/docs/intro',
             },
           ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+        }
       ],
       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
@@ -92,13 +62,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/NaroaCS/AutonomousBicycleSimulation',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
