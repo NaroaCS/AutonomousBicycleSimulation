1de8783eefbb521968b12278908d8e7408ceea5d
NaroaCS <naroa.coretti@gmail.com>
2022-04-22 12:05:24 -0400

    updated config and avoided rebalancing load if not needed

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from src.SimulationEngine import SimulationEngine
 from preprocessing.BikeGeneration import BikeGeneration
 
 config_path = os.path.join("data", "config.json")
-stations_path = os.path.join("data", "bluebikes_stations_07_2020.csv")
-users_path = os.path.join("data", "user_trips.csv")
 
 with open(config_path) as f:
     config = json.load(f)
 
+stations_path = os.path.join("data", "bluebikes_stations_07_2020.csv")
+users_path = os.path.join("data", "user_trips_"+ str(config["USER_TRIPS_FILE"]) +".csv")
+#users_path = os.path.join("data", "user_trips_0.csv")
+
+
+
 stations_data = BikeGeneration(config["NUM_BIKES"], config["MODE"], stations_path)
-users_data = pd.read_csv(users_path, nrows=800)
+users_data = pd.read_csv(users_path)
 
 city = SimulationEngine(config, stations_data, users_data)
 
 start = time.time()
-city.run(until=650000)
+city.run(until=1300000)
 print(time.time() - start)
